604e166c8552598fc4c2e7aeca255212a5e5e3d6
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-06-27 19:31:45 -0300

    Add files via upload

---
+++ Calculator_Python/Calculator_Python/Calculator_Python.py
@@ -0,0 +1,35 @@
+﻿import math
+from os import system
+
+while True:
+    operation = input("Enter your operation (+, -, *, /, source, high): ")
+    first = float(input("Enter your first number: "))
+
+    if operation == "+":
+        second = float(input("Enter your second number: "))
+        total = first + second
+    elif operation == "-":
+        second = float(input("Enter your second number: "))
+        total = first - second
+    elif operation == "*":
+        second = float(input("Enter your second number: "))
+        total = first * second
+    elif operation == "/":
+        second = float(input("Enter your second number: "))
+        if second != 0:
+            total = first / second
+        else:
+            total = "Division by zero is not allowed"
+    elif operation == "source":
+        total = math.sqrt(first)
+    elif operation == "high":
+        second = float(input("enter the number to be raised: "))
+        total = math.pow(first, second)
+    else:
+        total = "Please enter a valid operation"
+
+    print("Your operation result is:", total)
+
+    choice = input("Do you want to perform another operation? (y/n): ")
+    if choice.lower() != "y":
+        break
